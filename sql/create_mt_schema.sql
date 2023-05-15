142258d3cae5960b91ce8d5bb56853de774f1efe
GestaoMissTeen <gestaomissteen@gmail.com>
2023-05-14 23:39:37 -0300

    sql: added installments

--- sql/create_mt_schema.sql
+++ sql/create_mt_schema.sql
@@ -223,11 +223,11 @@ INSERT INTO tbl_payment_method(payment_method_name) VALUES
     ('Pix');
    
 INSERT INTO tbl_payment_method_installment(payment_method_id, payment_method_Installment_number) VALUES 
-	(1, 1),(1, 2),(1, 3),
-    (2, 1),
-    (3, 1),(3, 2),(3, 3),
-    (4, 1),
-    (5, 1);
+	(1, 1),(1, 2),(1, 3),(1, 4),(1, 5),(1, 6),(1, 7),(1, 8),(1, 9),(1, 10),
+    (2, 1),(2, 2),(2, 3),(2, 4),(2, 5),(2, 6),(2, 7),(2, 8),(2, 9),(2, 10),
+    (3, 1),(3, 2),(3, 3),(3, 4),(3, 5),(3, 6),(3, 7),(3, 8),(3, 9),(3, 10),
+    (4, 1),(4, 2),(4, 3),(4, 4),(4, 5),(4, 6),(4, 7),(4, 8),(4, 9),(4, 10),
+    (5, 1),(5, 2),(5, 3),(5, 4),(5, 5),(5, 6),(5, 7),(5, 8),(5, 9),(5, 10);
 
 CREATE TABLE tbl_sale(
 	sale_id INT NOT NULL AUTO_INCREMENT,
